d6f9bbe4c9a4af989aa74ecea94b0c5386a96c80
JoryKlaverstijn <63673224+JoryKlaverstijn@users.noreply.github.com>
2021-11-29 14:17:00 +0100

    Added all base code and finished assignment 1, 2

---
+++ Task_1/task1.m
@@ -0,0 +1,31 @@
+features = load("task_1.mat").lab1_1;
+
+correlations = [];
+for i = 1:3
+    for j = 1:3
+        covariance = cov(features(:, i), features(:, j));
+        covariance = covariance(1, 2);
+        correlation = covariance / (std(features(:, i)) * std(features(:, j)));
+        correlations(i, j) = correlation;
+    end
+end
+
+disp(correlations);
+
+height = features(:, 1);
+weight = features(:, 2);
+age = features(:, 3);
+
+% Plot the height vs. weight scatter plot
+figure()
+scatter(height, weight)
+xlabel('height (cm)')
+ylabel('weight (kg)')
+title('Plot A: height vs. weight')
+
+% Plot the weight vs age scatter plot
+figure()
+scatter(weight, age)
+xlabel('weight (kg)')
+ylabel('age')
+title('Plot B: weight vs. age')
