a53842e4f05b2d30b651ccf4e967f8ac30dcd197
Sparvriend <timowahl@live.nl>
2021-12-01 11:42:16 +0100

    Made one small change, Task 12 done now

--- Task_12/stat_analysis.m
+++ Task_12/stat_analysis.m
@@ -1,10 +1,10 @@
 % Function that calculates the quantization error 
-function q_error = stat_analysis(it, datapoints, print_types)
+function q_error = stat_analysis(it, datapoints, plus_plus_flag, print_types)
 q_error = (it);
 
 % The k_means algorithm is run iteration times, with k = 100
 for i=1:it
-    [class_labels, means] = k_means(100, datapoints, true, print_types);
+    [class_labels, means] = k_means(100, datapoints, plus_plus_flag, print_types);
     for p=1:length(datapoints)
         datapoint = [datapoints(p, 1) datapoints(p, 2)];
         mean_datapoint = [means(class_labels(p), 1) means(class_labels(p), 2)];
