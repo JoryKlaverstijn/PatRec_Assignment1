c9493d543be1ecfa5d35f1c3220ec6c16ea60419
Tsoutsouri <stella@wearespindle.com>
2021-12-14 19:55:44 +0100

    task 7 updates

---
+++ Task_7/lvq_algorithm.m
@@ -0,0 +1,17 @@
+load data_lvq_A.mat;
+load data_lvq_B.mat;
+
+%Perfom prototyping
+
+
+learning_rate = 0.01;
+trainSet =
+epochs =
+
+%After each epoch we determine the number of missclassified training
+%examples
+misclass_train =
+
+%We define the training error by dividing the misclass_train with the total
+%number of data points
+training_error = 
